e9af91a3fba402a78317afb353c700a36cd5f89b
Nazmul Hossain Pappu <mdpappu006@gmail.com>
2021-07-06 17:45:43 +0600

    Outputing v-for loop in vueJs.

---
+++ 01 Basic/07-outputing v-loop/main.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            courses : [
+                {
+                    name : ' VueJS',
+                    duration: '8 months',
+                    instructor: 'Nazmul Hossain'
+                },
+                
+                {
+                    name : ' Full-stack Development',
+                    duration: '24 months',
+                    instructor: 'Nazmul Hossain'
+                },
+
+                {
+                    name : ' front-end Development',
+                    duration: '12 months',
+                    instructor: 'Nazmul Hossain'
+                },
+
+                {
+                    name : ' PHP Laravel',
+                    duration: '15 months',
+                    instructor: 'Nazmul Hossain'
+                }
+            ]
+        }
+    },
+})
+
+app.mount("#app")
