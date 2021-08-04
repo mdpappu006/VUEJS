817c546313d1ab3905ef5e042232743ecb003ba4
Nazmul Hossain Pappu <mdpappu006@gmail.com>
2021-08-04 13:03:22 +0600

    Components and Modal.

--- 02 CLI/01/vueproject/src/App.vue
+++ 02 CLI/01/vueproject/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <h1>This is my first Hello world</h1>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Modal />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+import Modal from './components/Modal.vue'
+
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, Modal
   }
 }
 </script>
