1f0b076dfa1e1253cd092925f748ea7efb9f2132
Nazmul Hossain Pappu <mdpappu006@gmail.com>
2021-09-06 08:34:16 +0600

    Forms & data binding

---
+++ 04 Forms & Data Binding/web-form/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <signupForm />
+</template>
+
+<script>
+
+import signupForm from './components/signupForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    signupForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body{
+  margin:0;
+  background:#eee;
+}
+
+
+</style>
